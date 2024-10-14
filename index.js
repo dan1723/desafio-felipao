a060551e051cf6fd0eedf11a5e7e2be2772ae2e6
Danilo F Gomes <dan.gomes1723@gmail.com>
2024-10-14 10:10:46 -0300

    Projeto concluido

---
+++ index.js
@@ -0,0 +1,40 @@
+let nome = "kratos"
+let XP = 2001
+
+
+if (XP < 1000){
+    let nivel1 = "Ferro";
+    console.log(`O Herói de nome ${nome} está no nível ${nivel1}`);
+} else if (XP >= 1001 && XP <= 2000){
+    let nivel2 = "Bronze";
+    console.log(`O Herói de nome ${nome} está no nível ${nivel2}`);
+} else if (XP >= 2001 && XP <= 5000){
+    let nivel3 = "Prata";
+    console.log(`O Herói de nome ${nome} está no nível ${nivel3}`);
+} else if (XP >= 5001 && XP <= 7000){
+    let nivel4 = "Ouro";
+    console.log(`O Herói de nome ${nome} está no nível ${nivel4}`);
+} else if (XP >= 7001 && XP <= 8000){
+    let nivel5 = "Platina";
+    console.log(`O Herói de nome ${nome} está no nível ${nivel5}`);
+} else if (XP >= 8001 && XP <= 9000){
+    let nivel6 = "Ascendente";
+    console.log(`O Herói de nome ${nome} está no nível ${nivel6}`);
+} else if (XP >= 9001 && XP <= 1000){
+    let nivel7 = "Imortal";
+    console.log(`O Herói de nome ${nome} está no nível ${nivel7}`);
+} else if (XP >= 10001){
+    let nivel8 = "Radiante";
+    console.log(`O Herói de nome ${nome} está no nível ${nivel8}`);
+}
+        
+
+    
+
+    
+
+        
+    
+
+
+
